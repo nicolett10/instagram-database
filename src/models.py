99d951d3f7172d6597d7d0a236dc2b68f7ea0c16
nicolett10 <torrestapia.n@gmail.com>
2022-10-06 21:59:29 +0000

    instagram

--- src/models.py
+++ src/models.py
@@ -16,22 +16,37 @@ class User(Base):
     lastname = Column (String(30), nullable=False) 
     email = Column (String(50), nullable=False) 
     password = Column (String(10), nullable=False)
-
-class Comment(Base):
-    __tablename__ = 'comments'
-    id = Column (Integer, primary_key = True)
-
-class Post(Base):
-    __tablename__ = 'posts'
-    id = Column (Integer, primary_key = True)   
-
-class Media (Base):
-    __tablename__ = 'medias'
-    id = Column (Integer, primary_key = True)
+    follower = relationship('Follower', backref='User')
+    post = relationship('Post', backref ='User')
 
 class Follower(Base):
     __tablename__ = 'followers'
     id = Column (Integer, primary_key = True)
+    user_from_id = Column (Integer, ForeignKey('users.id'), nullable=False)
+    user_to_id = Column (Integer, ForeignKey('users.id'), nullable=False)
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column (Integer, primary_key = True) 
+    title = Column(String(100), unique=True, nullable=False)
+    user_id = Column (Integer, ForeignKey('users.id'), nullable=False)
+    comment = relationship('Comment')
+    media = relationship('Media')
+   
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column (Integer, primary_key = True)
+    comment_text = Column (String(150), nullable=False)
+    users_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    posts_id = Column(Integer, ForeignKey('posts.id'), nullable=False)
+
+class Media (Base):
+    __tablename__ = 'medias'
+    id = Column (Integer, primary_key = True)
+    type = Column (String(50), nullable=False) 
+    url = Column (String(50), nullable=False) 
+    post_id = Column (Integer, ForeignKey('posts.id'), nullable=False)
 
 
 ## Draw from SQLAlchemy base
